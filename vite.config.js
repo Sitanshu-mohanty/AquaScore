2f819286e2b2611dfe82806022e49a59913f6f35
Sitanshu sekhar mohanty <sitanshumohanty8@gmail.com>
2025-11-04 22:24:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Update this with your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/AquaScore/',
-});
+  base: '/aquascore/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
+})
